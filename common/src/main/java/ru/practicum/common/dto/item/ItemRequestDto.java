d45792c30389fb984e26a95a0cfc14abf28b540b
tubashov <tubashov@yandex.ru>
2025-12-11 15:07:44 +0300

    Update ItemRequestDto

--- common/src/main/java/ru/practicum/common/dto/item/ItemRequestDto.java
+++ common/src/main/java/ru/practicum/common/dto/item/ItemRequestDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.common.dto.item;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
